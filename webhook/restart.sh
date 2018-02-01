64b03e69f4580b2bbed3661ecedfbc6e9727e229
徐人杰 <2578370399@qq.com>
2018-02-01 10:48:21 +0800

    :sparkles: feat restart

--- webhook/restart.sh
+++ webhook/restart.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cnpm i
-NODE_ARGV_OPT=get_roll_article NODE_ARGV_3=5  pm2 start index.js
+NODE_ARGV_OPT=get_roll_article NODE_ARGV_3=5  pm2 restart index.js
 pm2 restart webhook/index.js
